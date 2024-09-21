579e70cecd07a6e4befe3441807c5edd42ed517b
BrAvE1SpiRiT <99087986+BrAvE1SpiRiT@users.noreply.github.com>
2024-09-21 15:13:58 +0500

    commmmm

--- application/__main__.py
+++ application/__main__.py
@@ -7,12 +7,3 @@ if __name__ == "__main__":
     import uvicorn
 
     is_debug = os.getenv("DEBUG") == "True"
-
-    uvicorn.run(
-        "application.run:app",
-        host="0.0.0.0",
-        port=5000,
-        access_log=True,
-        log_config=SETTINGS_LOGGER,
-        reload=is_debug,
-    )
